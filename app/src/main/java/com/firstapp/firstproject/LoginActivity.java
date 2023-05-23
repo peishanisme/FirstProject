df747dc2764c6ec2f64808ce25c5cb9dd2bacf10
user <22004795@siswa.um.edu.my>
2023-05-23 09:59:28 +0800

    debug

--- app/src/main/java/com/firstapp/firstproject/LoginActivity.java
+++ app/src/main/java/com/firstapp/firstproject/LoginActivity.java
@@ -64,7 +64,10 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.makeText(LoginActivity.this,"You are logged in successfully",Toast.LENGTH_SHORT).show();
 
                         loadingBar.dismiss();
-                        SendUserToMainActivity();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
                     }
                     else {
                         String message =task.getException().getMessage();
--- app/src/main/java/com/firstapp/firstproject/MainActivity.java
+++ app/src/main/java/com/firstapp/firstproject/MainActivity.java
@@ -1,15 +1,61 @@
 package com.firstapp.firstproject;
 
+import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
+import android.view.Menu;
+import com.firstapp.firstproject.databinding.ActivityMainBinding;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
+//    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+//            switch(item.getItemId()){
+//                case R.id.home:
+//                    replaceFragment(new HomeFragment());
+//                    break;
+//                case R.id.new_post:
+//                    break;
+//                case R.id.profile:
+//                    replaceFragment(new ProfileFragment());
+//                    break;
+//            }
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (id == R.id.new_post) {
+                // Handle another case
+            } else if(id==R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            }
+
+            return true;
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+
     }
 }
