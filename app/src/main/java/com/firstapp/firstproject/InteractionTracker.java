8bc1b28d90e04a20b5d212a87d414336eb712308
rtttsssz <rodericktingtohshan@gmail.com>
2023-06-18 23:34:18 +0800

    update with comment

--- app/src/main/java/com/firstapp/firstproject/InteractionTracker.java
+++ app/src/main/java/com/firstapp/firstproject/InteractionTracker.java
@@ -9,16 +9,16 @@ public class InteractionTracker {
     public InteractionTracker() {
     }
 
-    //Linked list is be used to store interaction
+    // Linked list is be used to store interaction
     static LinkedList <String> interactionList = new LinkedList<>();
     static LinkedList <String> interactionLinkList = new LinkedList<>();
     static LinkedList <String> interactionTimeList = new LinkedList<>();
 
     public static void add(String interaction, String interactionLink){
-        interactionList.add(interaction); //add interaction's name
-        interactionLinkList.add(interactionLink); //add interaction's link like uid of the user
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { //get the local time
-            interactionTimeList.add(LocalDateTime.now().toString());
+        interactionList.add(interaction); // add interaction's name
+        interactionLinkList.add(interactionLink); // add interaction's link like uid of the user
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // get the local time
+            interactionTimeList.add(LocalDateTime.now().toString()); // add time in the string form
         }
 
     }
@@ -28,7 +28,7 @@ public class InteractionTracker {
     }
 
 
-    public static void clear(){ //clear all the linked list
+    public static void clear(){ // clear all the linked list
         interactionList.clear();
         interactionTimeList.clear();
         interactionLinkList.clear();
